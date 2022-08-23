9132114d176d07e485e6372541838573d75fa06a
A.Kiumarsi.ir <A.Kiumarsi.ir@gmai.com>
2022-08-23 13:05:22 +0430

    add first

---
+++ JS/person.js
@@ -0,0 +1,14 @@
+function form() {
+    let x = document.forms["me"]["ss"].value;
+    if (x == "") {
+        alert("not ok")
+        return false;
+    }else{
+      alert("ok sended")
+    }
+
+
+   
+}
+
+
